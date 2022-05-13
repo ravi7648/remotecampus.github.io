620bfab09e5b2feefb44d328ec469e2e363e1697
ravi7648 <ravishankar123yadav@gmail.com>
2022-05-13 10:25:12 +0530

    updated review.php

--- review.php
+++ review.php
@@ -3,14 +3,14 @@ if (isset($_POST['submit'])) {
     $rname = $_POST['name'];
     $rscore = $_POST['score'];
     $rcomment = $_POST['comment'];
-    $servername = "192.168.0.105";
-    $username = "root";
-    $password = "password";
-    $dbname = "project_db";
+    $servername = "remotemysql.com:3306";
+    $username = "7q3V98dZRp";
+    $password = "I76tumo6iI";
+    $dbname = "7q3V98dZRp";
 
     if (!empty($rname) || !empty($rscore) || !empty($rcomment)) {
         $conn = new mysqli($servername, $username, $password, $dbname);
-        $sql = "INSERT INTO project_review (name, score, comment) VALUES ('$rname', '$rscore', '$rcomment')";
+        $sql = "INSERT INTO feedback (name, score, comment) VALUES ('$rname', '$rscore', '$rcomment')";
 
         if ($conn->query($sql) === TRUE) {
             echo "<script>alert('Thank you, for reviewing!');location.href='index.html'</script>";
